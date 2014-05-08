2025d83aee22ed50fd744d9cbd1e7b8625389d3d
aedwards12 <anthony.edwardsjr@gmail.com>
2014-05-08 13:15:30 -0400

    updated my_solution.js

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,29 +1,52 @@
 // I paired [by myself, with:] on this challenge.
 
-
+// I work by myself for this challenge
 
 
 // Pseudocode
-// 
-// 
-// 
-// 
-
+// make secretNumber a number
+// make secretNumber value 7
+// the value of password should be a string
+// variable passworkd should say 'just open the door'
+// allowedIn shoud be a boolean (true)
+// change allowedIn to equal false
+// make members a variable
+// make members an array
+// first member should be 'John'
+// fourth member should be 'Mary'
 
 // __________________________________________
 // Write your code below.
 
+//var secretNumber = 9
+var secretNumber = 7
 
+// var password = "password"
 
+var password = "just open the door"
 
+//var allowedIn = true
 
+var allowedIn = false
 
+var members
+//members = new.Array
+members = new Array
+
+members[0] = "John"
+members[3] = 'Mary'
 
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
 
 
-
+var secretNumber = 7
+var password = "just open the door"
+var allowedIn = false
+var members
+members = new Array
+members[0] = "John"
+members[3] = 'Mary'
 
 
 // __________________________________________
@@ -34,10 +57,17 @@
 // 
 // 
 // 
+U1.W3: JavaScript Variables and Object Types:
 
+For this challenge I worked by myself. Once I fully understood how this challenge worked I enjoyed it. 
+I had a tough time installing node on my mac. Somewhere along the line I missed the directions on how to 
+install it. I ended up googling tutorials on how to install node via terminal commands. This challege provided
+ a good exercise on making various variable objects and arrays. It was nice to add object to that array in 
+ specified locations within the array. Im curious to learn the how this tutorial was made. I did not find 
+ this challenge tedious. This was much better than the second challenge. I did not learn too much on this 
+ challege but got more practice declaring various objects.  I am confident with the learning competencies of
+  this challenge.
 
-// __________________________________________
-// Driver Code:  Do not alter code below this line.
 
 function assert(test, message, test_number) {
   if (!test) {
