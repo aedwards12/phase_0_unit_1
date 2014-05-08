ca5fadfea2fc49c196efc76f2e43ef7078aea01e
aedwards12 <anthony.edwardsjr@gmail.com>
2014-05-08 12:43:31 -0400

    updated my_solution.js

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,5 +1,5 @@
 // I worked on this challenge [by myself, with:]
- 
+ fahia mohamed
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
